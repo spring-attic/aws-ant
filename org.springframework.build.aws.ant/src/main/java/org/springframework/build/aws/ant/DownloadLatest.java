4a0468e08f0083c7011f221c01ad086b3d18ff76
Ben Hale <bhale@vmware.com>
2010-05-05 10:35:37 +0100

    Added a downloadLatest task

--- org.springframework.build.aws.ant/src/main/java/org/springframework/build/aws/ant/DownloadLatest.java
+++ org.springframework.build.aws.ant/src/main/java/org/springframework/build/aws/ant/DownloadLatest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2007-2008 the original author or authors.
+ * Copyright 2010 SpringSource
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,7 @@ package org.springframework.build.aws.ant;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.tools.ant.BuildException;
@@ -28,27 +29,16 @@ import org.jets3t.service.model.S3Bucket;
 
 /**
  * A member of the S3 ANT task for dealing with Amazon S3 download behavior.
- * This operation will use the credentials setup in its parent S3 task tag.
+ * This operation will use the credentials setup in its parent S3 task tag and
+ * download the latest matching file.
  * 
  * @author Ben Hale
  */
-public class Download extends AbstractS3DownloadOperation {
-
-	private String file;
+public class DownloadLatest extends AbstractS3DownloadOperation {
 
 	private List<FileSet> fileSets = new ArrayList<FileSet>(1);
 
 	private File toDir;
-
-	private File toFile;
-
-	/**
-	 * Optional parameter that corresponds to the source object key in S3
-	 * @param file The source object key in S3
-	 */
-	public void setFile(String file) {
-		this.file = file;
-	}
 
 	/**
 	 * Adds an optional fileSet to read files from.
@@ -67,55 +57,22 @@ public class Download extends AbstractS3DownloadOperation {
 	}
 
 	/**
-	 * Required parameter that corresponds to the file to download
-	 * @param toFile The file to download
-	 */
-	public void setToFile(File toFile) {
-		this.toFile = toFile;
-	}
-
-	/**
 	 * Verify that required parameters have been set
 	 */
 	public void init() {
 		if (bucketName == null) {
 			throw new BuildException("bucketName must be set");
 		}
-		if (file != null && fileSets.size() > 0) {
-			throw new BuildException("Only one of file and <fileset> may be set");
+		if (fileSets.size() == 0) {
+			throw new BuildException("At least one <fileset> must be set");
 		}
-		if (file == null && fileSets.size() == 0) {
-			throw new BuildException("At least one of file and <fileset> must be set");
-		}
-		if (toFile != null && toDir != null) {
-			throw new BuildException("Only one of toFile and toDir may be set");
-		}
-		if (toFile == null && toDir == null) {
-			throw new BuildException("At least one of toFile and toDir must be set");
-		}
-		if (fileSets.size() > 0 && toFile != null) {
-			throw new BuildException("toFile cannot be used when specifying a <fileset> to download");
+		if (toDir == null) {
+			throw new BuildException("toDir must be set");
 		}
 	}
 
 	public void execute(S3Service service) throws S3ServiceException, IOException {
-		if (file != null && toFile != null) {
-			processFileToFile(service);
-		}
-		else if (file != null && toDir != null) {
-			processFileToDir(service);
-		}
-		else if (fileSets.size() > 0 && toDir != null) {
-			processSetToDir(service);
-		}
-	}
-
-	private void processFileToFile(S3Service service) throws S3ServiceException, IOException {
-		getFile(service, getOperationBucket(), file, toFile);
-	}
-
-	private void processFileToDir(S3Service service) throws S3ServiceException, IOException {
-		getFile(service, getOperationBucket(), file, new File(toDir, file.substring(file.lastIndexOf('/'))));
+		processSetToDir(service);
 	}
 
 	private void processSetToDir(S3Service service) throws S3ServiceException, IOException {
@@ -124,11 +81,11 @@ public class Download extends AbstractS3DownloadOperation {
 			S3Scanner scanner = getS3Scanner(bucket, fileSet.mergePatterns(project), getS3SafeDirectory(fileSet
 					.getDir()));
 			List<String> keys = scanner.getQualifiyingKeys(service);
-			for (String key : keys) {
-				if (!key.endsWith("/")) {
-					getFile(service, bucket, key, new File(toDir, key.substring(getS3SafeDirectory(fileSet.getDir())
-							.length())));
-				}
+			Collections.sort(keys);
+			String key = keys.get(keys.size() - 1);
+			if (!key.endsWith("/")) {
+				getFile(service, bucket, key, new File(toDir, key.substring(getS3SafeDirectory(fileSet.getDir())
+						.length())));
 			}
 		}
 	}
